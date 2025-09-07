20dd3073668d93d75fe82f4128bb0edf5f1ddbfe
YASIKA.IT21 <yasika.it21@bitsathy.ac.in>
2025-09-07 23:25:07 +0530

    get,use,post

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use("/test",(req,res)=>{
+app.use("/abi", (req,res)=>{
     res.send("Hello World");
 })
+app.get("/abi", (req,res)=>{
+    res.send("Hello World test 1");
+})
+app.use("/test", (req,res)=>{
+    res.send("Hello World test");
+})
+// //app.use("/test", (req,res)=>{
+//     res.send("Hello World");
+// })
+// app.use("/", (req,res)=>{
+//     res.send("Hello World");
+// })
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
