075902c48043892e2a9e0d971809c0cb9c241533
YASIKA.IT21 <yasika.it21@bitsathy.ac.in>
2025-09-09 22:20:47 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use("/abi", (req,res)=>{
-    res.send("Hello World");
+app.use("/admin", (req,res,next)=>{
+    const token = "xyz";
+    const authorized = token === "xyz"; // Example check
+    if (authorized) {
+        //res.status(200).send("Admin Authorized");
+        console.log("Admin Authorized");
+        next();
+    } else {
+        res.status(403).send("Forbidden");
+    }
 })
-app.get("/abi", (req,res)=>{
+app.get("/admin/delete_admin", (req,res)=>{
     res.send("Hello World test 1");
 })
-app.use("/test", (req,res)=>{
-    res.send("Hello World test");
-})
-// //app.use("/test", (req,res)=>{
-//     res.send("Hello World");
-// })
-// app.use("/", (req,res)=>{
-//     res.send("Hello World");
-// })
+
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
