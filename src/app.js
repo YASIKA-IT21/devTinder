0ecce2c4daac1b11dd801f8e459776819ed41d34
YASIKA.IT21 <yasika.it21@bitsathy.ac.in>
2025-09-14 23:56:30 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -14,6 +14,30 @@ app.post('/user',async(req,res)=>{
         res.status(500).json({message:"Internal server error",error:err.message});
     }
 })
+//FEED API=to get all users from the database
+app.get('/feed',async(req,res)=>{
+    const email = req.body.email;
+      
+    try{
+      
+        const  users=   await user.findOne({email:email});
+        res.send(users);
+        
+
+    }
+    catch(err){
+        res.status(400).json({message:"Error fetching users"});
+    }
+})
+app.delete('/delete',async(req,res)=>{
+    const userid = req.body.userid;
+    try{
+        const deleteduser = await user.findByIdAndDelete(userid);
+        res.status(200).json({message:"User deleted successfully",user:deleteduser});
+    }catch(err){
+        res.json({message:"Internal server error",error:err.message});
+    }
+})
 const PORT = process.env.PORT || 3000;
 database().then(()=>
     {console.log("Database connected");app.listen(PORT,()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     firstName:{
         type:String,
+        required:true,
+        minLength:2,
+        maxLength:30
     },
 
     lastName:{
@@ -10,22 +13,43 @@ const userSchema = mongoose.Schema({
 
     email:{
         type:String,
+        required:true,
+        unique:true,
+        lowercase:true
         
-    },
+    }, 
 
     password:{
         type:String,
-        
+        required:true
     },
 
     age:{
         type:Number,
+        min:18
         
     },
     gender:{
         type:String,
-    }
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender is not valid");
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-2210.jpg?semt=ais_incoming&w=740&q=80"
 
+    },
+    about:{
+        type:String,
+        default:"This is a default value"
+    },
+
+
+},{
+    timestamps:true,
 })
 const user = new mongoose.model('user',userSchema);
 module.exports=user;
